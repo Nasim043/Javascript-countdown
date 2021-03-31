d168b09f78f6b71b78b861ced5b9a0d9120b1a25
Saleh Nasim <nasimcse16@gmail.com>
2021-03-31 19:33:42 +0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
+const Mybday = '7 jan 2022';
+
+function countdown() {
+    const nextYearsBday = new Date(Mybday);
+    const currentDate = new Date();
+    const totalSeconds = (nextYearsBday - currentDate) / 1000;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor((totalSeconds / 3600) % 24);
+    const mins = Math.floor((totalSeconds / 60) % 60);
+    const seconds = Math.floor(totalSeconds % 60);
+
+    daysEl.innerHTML = formatTime(days);
+    hoursEl.innerHTML = formatTime(hours);
+    minutesEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+// initial call
+countdown();
+setInterval(countdown, 1000);
